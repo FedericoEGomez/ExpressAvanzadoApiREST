469c642432fc9eec94aae73693855819090aab92
fedegox <fedegox775@gmail.com>
2021-08-14 18:58:12 -0300

    tarea coder house

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -1,24 +1,4 @@
-let productos = [
-    {
-        id: 1,
-        title: 'computadora',
-        price: 123,
-        thumbnail: 'url'
-    },
-    {
-        id: 2,
-        title: 'heladera',
-        price: 1234,
-        thumbnail: 'url'
-    },
-    {
-        id: 3,
-        title: 'cocina',
-        price: 12345,
-        thumbnail: 'url'
-    },
-    
-    ]
+let productos = require ('../productos/productos')
 
 //obtener los productos
 const productosGet = (req,res) => {
--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-
 class Server {
     constructor(){
         this.app = express();
